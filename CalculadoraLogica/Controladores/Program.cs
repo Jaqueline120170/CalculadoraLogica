ce658517c42526f272739b2f5ae46229e7b3829a
Jaqueline Romero <csi23-jromres@colegioaltair.net>
2023-10-15 22:10:46 +0200

    ejercicio resuelto

---
+++ CalculadoraLogica/Controladores/Program.cs
@@ -0,0 +1,57 @@
+﻿using CalculadoraLogica.Servicios;
+
+namespace CalculadoraLogica.Controladores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuInterfaz mi = new MenuImplementacion();
+            bool cerrarMenu = false;
+
+            while (!cerrarMenu)
+            {
+                int opcion = mi.mostrarMenu();
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("¡Hasta pronto!");
+                        cerrarMenu = true;
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Ha elegido Igualdad");
+                        Console.WriteLine("Introduce una palabra: ");
+                        string palabra1 = Console.ReadLine();
+                        Console.WriteLine("Introduce una palabra: ");
+                        string palabra2 = Console.ReadLine();
+                        if (palabra1 == palabra2)
+                        {
+                            Console.WriteLine("El resultado de la igualdad es: True");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El resultado de la igualdad es: False");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Ha elegido Desigualdad");
+                        Console.WriteLine("Introduce una palabra: ");
+                        palabra1 = Console.ReadLine();
+                        Console.WriteLine("Introduce una palabra: ");
+                        palabra2 = Console.ReadLine();
+                        if (palabra1 != palabra2)
+                        {
+                            Console.WriteLine("El resultado de la desigualdad es: True");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El resultado de la desigualdad es: False");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ CalculadoraLogica/Servicios/MenuImplementacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraLogica.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public int mostrarMenu()
+        {
+            Console.WriteLine("###########");
+            Console.WriteLine("1. Cerrar");
+            Console.WriteLine("2. Igualdad");
+            Console.WriteLine("3. Desigualdad");
+            Console.WriteLine("Selecciona una opción: ");
+            int opcion = Console.ReadKey(true).KeyChar-('0');
+            Console.WriteLine("La opción seleccionada es: " + opcion);
+
+            return opcion;
+        }
+    }
+}
